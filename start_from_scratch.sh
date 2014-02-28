e6138f1da594aa461df16826fa35f33998a3d9ce
Slava Kim <slava@meteor.com>
2014-02-27 17:22:06 -0800

    adf

--- start_from_scratch.sh
+++ start_from_scratch.sh
@@ -8,14 +8,14 @@ sleep 2
 
 cat << EOF | ./mongo
 
-db.people.insert({ name: "Slava Kim", address: { city: "SF", state: "CA"  }  })
+db.people.insert({ name: "John Doe", address: { city: "SF", state: "CA"  }  })
 
 db.places.insert({ _id: "A", ratings: [5, 1, 3] })
 db.places.insert({ _id: "B", ratings: [2, 4, 9] })
 db.places.insert({ _id: "C", ratings: [1] })
 
-db.restaurants.insert({ _id: "X", locations: [[1, 2], [1, 0]] })
-db.restaurants.insert({ _id: "Y", locations: [[0, 0]] })
+db.restaurants.insert({ _id: "A", locations: [[1, 2], [1, 0]] })
+db.restaurants.insert({ _id: "B", locations: [[0, 0]] })
 db.restaurants.ensureIndex({ locations: "2d" })
 
 
